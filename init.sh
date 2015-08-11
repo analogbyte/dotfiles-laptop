0f44a0fac01947d34bf0f91ebf7eb632ac1b71d4
Daniel Nägele <code@danieln.de>
2015-08-11 16:08:12 +0200

    redisgin all

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ src_dir="`( cd \"$src_dir\" && pwd )`"  # absolutized and normalized path
 
 ln -snf $src_dir/i3 ~/.i3
 ln -snf $src_dir/dunstrc ~/.config/dunstrc
-ln -snf $src_dir/Xdefaults ~/.Xdefaults
+ln -snf $src_dir/Xresources ~/.Xresources
